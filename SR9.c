9402426d210cbdf7377cc2ca4d0cebb042a3d708
Yaskovich Dmitry <kadavr95@outlook.com>
2012-12-08 01:05:00 +0300

    -

--- SR9.c
+++ SR9.c
@@ -5,15 +5,15 @@ int recur(int kk);
 int main(void) {
 int k;
 printf("Input numbers:");
-scanf("%d %d", &m, &n);
-printf("Function=%ld\n", recur(m,n));
+scanf("%d", &k);
+printf("Function=%d\n", recur(k));
 fflush(stdin);
 getchar();
 return 0;
 }
 
-int recur(int mm,int nn) {
- printf("%d %d\n",mm,nn);
+int recur(int kk) {
+ //printf("%d %d\n",mm,nn);
 if ((mm==0)||(nn==0))
  {
    return (nn+1);
